0e4446e3148684b7e08ed4903eede9dd9f46f1db
Francisco Varela Cid <fvarelacid@gmail.com>
2022-08-30 15:42:31 +0100

    Created Main Js & Added messages Object

---
+++ MixedMessages/main.js
@@ -0,0 +1,6 @@
+const messages = {
+    'noun': ['Anna', 'Olivia', 'Luisa', 'Flora', 'Georgie', 'Meredith', 'Barnaby', 'Julian', 'Filipe', 'Tomas', 'Salvador', 'Luisa', 'Francisco'],
+    'verb': ['is', 'snores', 'stinks', 'cries', 'agrees', 'holds', 'lends', 'hides', 'hits', 'dives', 'drops', 'does', 'farts'],
+    'adjective': ['a cunty', 'at the beach', 'in the bum', 'at the lake', 'in Mars', 'a melon', 'all over the place', 'down the isle', 'blunt as Olivia']
+};
+
