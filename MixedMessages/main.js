12537890ce2cfc8112c3e74978b200fe4471a9c9
Francisco Varela Cid <fvarelacid@gmail.com>
2022-08-30 16:32:37 +0100

    Created message generator

--- MixedMessages/main.js
+++ MixedMessages/main.js
@@ -1,6 +1,14 @@
 const messages = {
     'noun': ['Anna', 'Olivia', 'Luisa', 'Flora', 'Georgie', 'Meredith', 'Barnaby', 'Julian', 'Filipe', 'Tomas', 'Salvador', 'Luisa', 'Francisco'],
-    'verb': ['is', 'snores', 'stinks', 'cries', 'agrees', 'holds', 'lends', 'hides', 'hits', 'dives', 'drops', 'does', 'farts'],
-    'adjective': ['a cunty', 'at the beach', 'in the bum', 'at the lake', 'in Mars', 'a melon', 'all over the place', 'down the isle', 'blunt as Olivia']
+    'verb': [' is', ' snores', ' stinks', ' cries', ' agrees', ' holds', ' lends', ' hides', ' hits', ' dives', ' drops', ' does', ' farts'],
+    'adjective': [' a cunty', ' at the beach', ' in the bum', ' at the lake', ' in Mars', ' a melon', ' all over the place', ' down the isle', ' slut as Olivia']
 };
 
+let message = '';
+
+for (const part in messages) {
+    const item = messages[part][Math.floor(Math.random()*messages[part].length)];
+    message += item
+}
+
+console.log(message)
